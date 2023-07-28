f1f3a869cd65f755f8288c0c59b5f0c9782445ee
Suansh Budhiraja <ishaansharma750@gmail.com>
2023-07-28 20:09:51 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import './../styles/App.css';
-import Main from "./Main";
+import Parent from "./Parent";
+import Child from "./Child";
 
 const App = () => {
   return (
     <div>
-      <Main />
+      {/* Do not remove the main div */}
+      <Parent />
     </div>
   )
 }
---
+++ src/components/Parent.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import Child from "./Child";
+
+const Parent = () => {
+    let [inputValue, setInputvalue] = useState("")
+    return (
+        <div className="parent">
+            <h1>Parent Component</h1>
+            <div>{inputValue}</div>
+            <Child changeState={setInputvalue} />
+        </div>
+    )
+}
+
+export default Parent
